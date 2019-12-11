b0630c8560274029fe65332f242ef9a0d9971d2e
kishancd <kishan.cd@robosoftin.com>
2019-12-11 11:53:45 +0530

    modified:   interviewtracking/application.log 	modified:   interviewtracking/bin/main/application.properties 	modified:   interviewtracking/bin/main/com/robosoft/interviewtracking/controller/InterviewTrackingController.class 	deleted:    interviewtracking/bin/main/com/robosoft/interviewtracking/service/InterviewService.class 	deleted:    interviewtracking/bin/main/com/robosoft/interviewtracking/service/InterviewServiceImpl.class 	deleted:    interviewtracking/bin/main/db/migration/V1.1__candidateSql.sql 	deleted:    interviewtracking/bin/main/db/migration/V1.1__hrPanelSql.sql 	deleted:    interviewtracking/bin/main/db/migration/V1.3__techPanelSql.sql 	deleted:    interviewtracking/bin/main/db/migration/V1.4__skillsSql.sql 	deleted:    interviewtracking/bin/main/db/migration/V1.5__interviewProcessSql.sql 	deleted:    interviewtracking/bin/main/db/migration/V1__candidateSql.sql 	modified:   interviewtracking/bin/main/db/migration/V1__dataSql.sql 	modified:   interviewtracking/src/main/java/com/robosoft/interviewtracking/controller/CandidateController.java 	modified:   interviewtracking/src/main/resources/application.properties 	deleted:    interviewtracking/src/main/resources/db/migration/V1.1__candidateSql.sql 	deleted:    interviewtracking/src/main/resources/db/migration/V1.1__hrPanelSql.sql 	deleted:    interviewtracking/src/main/resources/db/migration/V1.3__techPanelSql.sql 	deleted:    interviewtracking/src/main/resources/db/migration/V1.4__skillsSql.sql 	deleted:    interviewtracking/src/main/resources/db/migration/V1.5__interviewProcessSql.sql 	deleted:    interviewtracking/src/main/resources/db/migration/V1__candidateSql.sql 	modified:   interviewtracking/src/main/resources/db/migration/V1__dataSql.sql

--- interviewtracking/src/main/java/com/robosoft/interviewtracking/controller/CandidateController.java
+++ interviewtracking/src/main/java/com/robosoft/interviewtracking/controller/CandidateController.java
@@ -3,39 +3,41 @@
 import java.util.List;
 import java.util.NoSuchElementException;
 
-import javax.mail.MessagingException;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.robosoft.interviewtracking.dto.*;
 import com.robosoft.interviewtracking.exception.CustomException;
 import com.robosoft.interviewtracking.service.CandidateService;
-import com.robosoft.interviewtracking.service.HRPanelService;
-import com.robosoft.interviewtracking.service.TechPanelService;
 
 
-@Controller("/")
-public class InterviewTrackingController {
+@Controller("/candidate")
+public class CandidateController {
 
 	@Autowired
 	CandidateService candidateService;
 	
-	@Autowired
-	TechPanelService techService;
 	
-	@Autowired
-	HRPanelService hrService;
+	@RequestMapping
+	@ResponseBody
+	public String test() {
+	System.out.println("==================app startup================");
+		return "App Started";
+	}
+	
 	
 	/* To add candidate */
-	@RequestMapping(value="candidate", method = RequestMethod.POST)
+	@PostMapping(value="candidate")
 	@ResponseBody
 	public ResponseEntity<Candidate> addCandidate(@RequestBody Candidate candidate)
 	{
@@ -43,7 +45,7 @@ public class InterviewTrackingController {
 	}
 	
 	/* To update candidate model */
-	@RequestMapping(value="candidate/{id}", method = RequestMethod.PUT)
+	@PatchMapping(value="candidate/{id}")
 	@ResponseBody
 	public ResponseEntity<Candidate> updateCandidate(@PathVariable("id") int id, @RequestBody Candidate candidate) 
 	
@@ -59,7 +61,7 @@ public class InterviewTrackingController {
 	
 	/* To delete skills */
 	@SuppressWarnings("rawtypes")
-	@RequestMapping(value = "candidate", method = RequestMethod.PUT)
+	@DeleteMapping(value = "candidate")
 	@ResponseBody
 	public ResponseEntity deleteSkills(@RequestHeader("id") int id, @RequestHeader("skills") String skills)
 	{
@@ -68,16 +70,11 @@ public class InterviewTrackingController {
 	}
 
 	
-	@RequestMapping
-	@ResponseBody
-	public String test() {
-	System.out.println("==================app startup================");
-		return "App Started";
-	}
+	
 	
 	/* To shortlist */
 	
-	@RequestMapping(value = "candidate", method = RequestMethod.GET)
+	@GetMapping(value = "candidate")
 	@ResponseBody 
 	public List<Candidate> getShortlistedCandidate(@RequestHeader("experience") int experience, @RequestHeader("skills") String skills)
 	{
@@ -85,57 +82,13 @@ public class InterviewTrackingController {
 	} 
 	
 	
-	/*To add technical panel */
-	@RequestMapping(value = "techPanel", method = RequestMethod.POST)
-	@ResponseBody 
-	public TechnicalPanel addTechnicalPanel(@RequestBody TechnicalPanel techPanelDto)
-	{
-		return techService.addTechnicalPanel(techPanelDto);
-	}
 	
 	
-	/* To add HR panel */
-	@RequestMapping(value = "hrPanel", method = RequestMethod.POST)
-	@ResponseBody 
-	public ResponseEntity<HRPanel> addHRPanel(@RequestBody HRPanel hrPanelDto)
-	{
-		return hrService.addHRPanel(hrPanelDto);
-	}
 	
 	
-	/* To send an email to candidate*/ 
-	@RequestMapping(value = "candidate/email", method = RequestMethod.POST)
-	@ResponseBody 
-	public void sendEmailToCandidate(@RequestBody MailDto mailDto)
-	{
-		 System.out.println("date = " +mailDto.getDate());
-		try {
-			hrService.sendEmailToCandidate(mailDto);
-		} catch (MessagingException e) {
-			throw new CustomException(100,"invalid mail");
-		}
-	}
-	
-	/* To shortlist panelists */
-	
-	@RequestMapping(value = "techPanel", method = RequestMethod.GET)
-	@ResponseBody 
-	public List<TechnicalPanel> getPanelists(@RequestHeader("panelId") String panelId, @RequestHeader("expertise") String expertise)
-	{
-		return techService.getPanelist(panelId, expertise);
-	}
 	
 	
-	/* To send an email to panelists*/
-	@RequestMapping(value = "techPanel/email", method = RequestMethod.POST)
-	@ResponseBody 
-	public void sendEmailToPanelists(@RequestBody MailDto mailDto)
-	{
-		 System.out.println("date = " +mailDto.getDate());
-		try {
-			hrService.sendEmailToCandidate(mailDto);
-		} catch (MessagingException e) {
-			throw new CustomException(100,"invalid mail");
-		}
-	}
+	
+	
+	
 }
