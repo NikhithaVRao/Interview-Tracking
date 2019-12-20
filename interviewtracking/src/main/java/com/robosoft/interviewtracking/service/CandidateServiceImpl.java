a7d98c41c8c726ae3cb0e2fbcfe772d15df00e0e
harshithasnand <harshithasnand98@gmail.com>
2019-12-20 10:27:35 +0530

    add

--- interviewtracking/src/main/java/com/robosoft/interviewtracking/service/CandidateServiceImpl.java
+++ interviewtracking/src/main/java/com/robosoft/interviewtracking/service/CandidateServiceImpl.java
@@ -67,7 +67,7 @@ public class CandidateServiceImpl implements CandidateService{
 		if(candidate.getCurrentSalary() != 0) {
 			cmodel.setCurrentSalary(candidate.getCurrentSalary());
 		}
-		
+		 
 		if(candidate.getGender() != null) {
 			cmodel.setGender(candidate.getGender());
 		}
